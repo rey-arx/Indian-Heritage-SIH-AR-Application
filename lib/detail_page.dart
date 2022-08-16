8a83bd280aab2b6fb252782c4302e34fb87f81af
rey-arx <raihan786arx@gmail.com>
2022-08-16 22:40:31 +0530

    Translation Completed

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,15 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_universe/data.dart';
 import 'constants.dart';
-import 'samplescenepage.dart'; //import 'package:flutter_unity_widget/flutter_unity_widget.dart';
+import 'samplescenepage.dart';
+import 'package:translator/translator.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   final PlanetInfo planetInfo;
+  DetailPage({Key key, @required this.planetInfo}) : super(key: key);
 
-  const DetailPage({Key key, this.planetInfo}) : super(key: key);
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  GoogleTranslator translator = GoogleTranslator();
+
+  List<String> items = [
+    "Select_a_language",
+    "English",
+    "Tamil",
+    "Hindi",
+    "Malayalam",
+  ];
+
+  String selectedItem = "Select_a_language";
+  String text = "hello";
 
   @override
   Widget build(BuildContext context) {
+    //below string used in translator
+    String cont = widget.planetInfo.description;
+
+    //when a dropdown value changes it will load
+    void trans(lan) async {
+      switch (lan) {
+        case "English":
+          print(cont);
+          await translator.translate(cont, to: "en").then((value) {
+            setState(() {
+              widget.planetInfo.description = value.toString();
+            });
+          });
+          break;
+        case "Tamil":
+          await translator.translate(cont, to: "ta").then((value) {
+            setState(() {
+              widget.planetInfo.description = value.toString();
+            });
+          });
+          break;
+        case "Hindi":
+          await translator.translate(cont, to: "hi").then((value) {
+            setState(() {
+              widget.planetInfo.description = value.toString();
+            });
+          });
+          break;
+        case "Malayalam":
+          await translator.translate(cont, to: "ml").then((value) {
+            setState(() {
+              widget.planetInfo.description = value.toString();
+            });
+          });
+          break;
+      }
+      //translator.translate(cont, to: "ta").then((value) {
+      //  setState(() {
+      //   widget.planetInfo.description = value.toString();
+      // });
+      //});
+    }
+
     return Scaffold(
       body: SafeArea(
         bottom: false,
@@ -23,7 +84,7 @@ class DetailPage extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        planetInfo.position.toString(),
+                        widget.planetInfo.position.toString(),
                         style: TextStyle(
                           fontFamily: 'Avenir',
                           fontSize: 247,
@@ -35,7 +96,7 @@ class DetailPage extends StatelessWidget {
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Image.asset(planetInfo.iconImage,
+                          Image.asset(widget.planetInfo.iconImage,
                               width: 230), //expected
                         ],
                       ),
@@ -48,7 +109,7 @@ class DetailPage extends StatelessWidget {
                       children: <Widget>[
                         SizedBox(height: 20),
                         Text(
-                          planetInfo.name,
+                          widget.planetInfo.name,
                           style: TextStyle(
                             fontFamily: 'Avenir',
                             fontSize: 56,
@@ -64,10 +125,33 @@ class DetailPage extends StatelessWidget {
                             PlainSUrfacePgaeRouter(context);
                           },
                         ),
+                        //DropdownMenu started.........
+                        DropdownButton(
+                            value: selectedItem,
+                            items: items.map((String items) {
+                              return DropdownMenuItem(
+                                value: items,
+                                child: Text(
+                                  items,
+                                  style: TextStyle(
+                                    fontFamily: 'Avenir',
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              );
+                            }).toList(),
+                            onChanged: (String newValue) {
+                              setState(() {
+                                selectedItem = newValue;
+                                trans(selectedItem);
+                              });
+                            }),
+                        //DropdownMenu ended.........
                         Divider(color: Colors.black38),
                         SizedBox(height: 32),
                         Text(
-                          planetInfo.description ?? '',
+                          widget.planetInfo.description ?? '',
                           maxLines: 590,
                           overflow: TextOverflow.ellipsis,
                           style: TextStyle(
@@ -99,7 +183,7 @@ class DetailPage extends StatelessWidget {
                     height: 100,
                     padding: const EdgeInsets.only(left: 32.0),
                     child: ListView.builder(
-                        itemCount: planetInfo.images.length,
+                        itemCount: widget.planetInfo.images.length,
                         scrollDirection: Axis.horizontal,
                         itemBuilder: (context, index) {
                           return Card(
@@ -110,7 +194,7 @@ class DetailPage extends StatelessWidget {
                             child: AspectRatio(
                                 aspectRatio: 1,
                                 child: Image.asset(
-                                  planetInfo.images[index],
+                                  widget.planetInfo.images[index],
                                   fit: BoxFit.cover,
                                 )),
                           );
@@ -140,7 +224,7 @@ class DetailPage extends StatelessWidget {
   void PlainSUrfacePgaeRouter(BuildContext context) {
     // UnityWidgetController _unityWidgetController;
 
-    if (planetInfo.position == 1) {
+    if (widget.planetInfo.position == 1) {
       print("111111111111111111111111111111111111111");
       //_unityWidgetController.postMessage("GameObject", "LoadScene", "1");
       //pageIndex = "1";
@@ -148,7 +232,7 @@ class DetailPage extends StatelessWidget {
         context,
         MaterialPageRoute(builder: (context) => SimpleScreen(pageIndex: "1")),
       );
-    } else if (planetInfo.position == 2) {
+    } else if (widget.planetInfo.position == 2) {
       print("22222222222222222222222222222222222222222222");
       //pageIndex = "2";
       //_unityWidgetController.postMessage("GameObject", "LoadScene", "2");
@@ -157,7 +241,7 @@ class DetailPage extends StatelessWidget {
         context,
         MaterialPageRoute(builder: (context) => SimpleScreen(pageIndex: "2")),
       );
-    } else if (planetInfo.position == 3) {
+    } else if (widget.planetInfo.position == 3) {
       print("33333333333333333333333333333333333333333333333");
       //pageIndex = "3";
       // _unityWidgetController.postMessage("GameObject", "LoadScene", "3");
@@ -165,60 +249,6 @@ class DetailPage extends StatelessWidget {
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => SimpleScreen(pageIndex: "3")),
-      );
-    } else if (planetInfo.position == 4) {
-      print("33333333333333333333333333333333333333333333333");
-      //pageIndex = "3";
-      // _unityWidgetController.postMessage("GameObject", "LoadScene", "3");
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => SimpleScreen(pageIndex: "4")),
-      );
-    } else if (planetInfo.position == 5) {
-      print("33333333333333333333333333333333333333333333333");
-      //pageIndex = "3";
-      // _unityWidgetController.postMessage("GameObject", "LoadScene", "3");
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => SimpleScreen(pageIndex: "5")),
-      );
-    } else if (planetInfo.position == 6) {
-      print("33333333333333333333333333333333333333333333333");
-      //pageIndex = "3";
-      // _unityWidgetController.postMessage("GameObject", "LoadScene", "3");
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => SimpleScreen(pageIndex: "6")),
-      );
-    } else if (planetInfo.position == 7) {
-      print("33333333333333333333333333333333333333333333333");
-      //pageIndex = "3";
-      // _unityWidgetController.postMessage("GameObject", "LoadScene", "3");
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => SimpleScreen(pageIndex: "7")),
-      );
-    } else if (planetInfo.position == 8) {
-      print("33333333333333333333333333333333333333333333333");
-      //pageIndex = "3";
-      // _unityWidgetController.postMessage("GameObject", "LoadScene", "3");
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => SimpleScreen(pageIndex: "8")),
-      );
-    } else if (planetInfo.position == 9) {
-      print("33333333333333333333333333333333333333333333333");
-      //pageIndex = "3";
-      // _unityWidgetController.postMessage("GameObject", "LoadScene", "3");
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => SimpleScreen(pageIndex: "9")),
       );
     }
   }
